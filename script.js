e0689692219e8fd8bde4e8363a972efd5680ee8d
Mohammadyasar Mansuri <126454357+Mohammadyasar303@users.noreply.github.com>
2025-02-11 11:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 //your JS code here. If required.
 let obj={
 	name:"name",
+	age:"22",
+	city:"city",
 };
 
 function getKeys(obj) {
 	return Object.keys(obj);
 }
 
-console.log(getKeys(obj))
+console.log(getKeys(obj));
